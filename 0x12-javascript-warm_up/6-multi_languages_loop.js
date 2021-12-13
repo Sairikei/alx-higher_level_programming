55681cc7033478b0bec5e10830e6ff11243d10d5
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-12-13 15:48:43 -0800

    javascript warmup

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
+// Print 3 lines using an array of string and a loop
 const myList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let myIndex = 0; my Index < 3; myIndex++) {
-	console.log(myList[myIndex]);
+for (let myIndex = 0; myIndex < 3; myIndex++) {
+  console.log(myList[myIndex]);
 }
