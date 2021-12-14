367c9292fdf0d04e0902aaf61faedf652d8e2ed8
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-12-13 16:03:40 -0800

    javascript warmup

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
-// Computes and prints a factorial
-function factorial (factor) {
-  if (factor === 0) {
-    return 0;
-  } else if (factor === 1) {
-    return 1;
+const a = process.argv[2];
+function factorial (a) {
+  if (isNaN(a) || a === 1) {
+    return (1);
   } else {
-    return (factor * factorial(factor -1));
+    return (a * factorial(a - 1));
   }
 }
-const myVar = parseInt(process.argv[2]);
-if (isNaN(myVar) || myVar === undefined) {
-  console.log('1');
-} else {
-  console.log(factorial(myVar));
-}
+console.log(factorial(parseInt(a)));
