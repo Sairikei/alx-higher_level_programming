e6532fad1a3c34c6b294ef34305a92871cf979ab
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-12-15 12:44:57 -0800

    js objects

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-	return function (nbase) {
-		return (nbase.toString(base));
-	};
+  return function (nbase) {
+    return (nbase.toString(base));
+  };
 };
