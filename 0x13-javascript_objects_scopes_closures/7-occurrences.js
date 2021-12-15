c12ab9d8036f0b371ffa02b590e9b3daf60a334d
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2021-12-15 12:41:58 -0800

    js objects

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-exports.nbOcurences = function (list, searchElement) {
-  let count = 0;
-  for (const el of list) {
-    if (el === searchElement) count++;
-  }
-  return count;
+
+exports.nbOccurences = function (list, searchElement) {
+	let count = 0;
+	for (const el of list) {
+		if (el === searchElement) count++;
+	}
+	return count;
 };
