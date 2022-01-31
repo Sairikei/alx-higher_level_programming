84ef41666eab31a5e45fbfa7e47d07064833929a
Sairikei <gedionsaiyuah@gmail.com>
2022-01-31 14:52:01 -0800

    fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""A script that takes in a URL,
-Sends a request to the URL,
-And displays the value of the X-Request-Id variable found in the header ofthe response.
-"""
-
+"""script that takes in a URL, sends a request to
+the URL and displays the value of the X-Request-Id
+variable found in the header of the response."""
 import sys
-import urllib.request
-
+from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as resp:
-        print(dict(resp.headers).get("X-Request-Id"))
+    req = Request(url)
+    with urlopen(req) as res:
+        print(dict(res.headers).get("X-Request-Id"))
