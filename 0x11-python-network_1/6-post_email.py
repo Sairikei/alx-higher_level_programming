a9a0a0c48e4d51c870666ecfcfac746063d95844
Sairikei <gedionsaiyuah@gmail.com>
2022-01-31 14:57:01 -0800

    fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-""" post email """
-import requests
+"""script that takes in a URL and an email, sends
+a POST request to the passed URL with the email as
+a parameter, and displays the body of
+the response (decoded in utf-8)"""
 import sys
-
+import requests
 
 if __name__ == "__main__":
-    email = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], data=email)
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {'email': email}
+
+    res = requests.post(url, data=values)
+    print(res.text)
