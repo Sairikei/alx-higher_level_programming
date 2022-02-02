0af989991be0b5d2e9028bec157f9007fa6bfb4c
Sairikei <gedionsaiyuah@gmail.com>
2022-02-02 15:37:50 -0800

    js webscrapping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,17 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
+const { argv } = require('process');
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    for (const filmIndex in films) {
-	    const filmChars = films[filmIndex].characters;
-	    for (const charIndex in filmChars) {
-        if (filmChars[charIndex].includes('18')) {
-		    count++;
-        }
-	    }
-    }
-    console.log(count);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+request(argv[2], (error, response, body) => {
+  if (error) {
+    console.error(error);
   }
+  let Count = 0;
+  const doc = JSON.parse(body).results;
+  doc.forEach(p => p.characters.forEach(msg => {
+    if (msg.slice(-3, -1) === '18') {
+      Count++;
+    }
+  }));
+  console.log(Count);
 });
